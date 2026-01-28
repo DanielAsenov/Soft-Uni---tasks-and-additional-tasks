b7bc2c7bf20bddfcf76ecfad417e1ad3740fb2da
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 21:14:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+string dayOFweek = Console.ReadLine();
+
+if (dayOFweek == "Monday" || dayOFweek == "Tuesday" || dayOFweek == "Wednesday" || dayOFweek == "Thursday" || dayOFweek == "Friday")
+{
+    Console.WriteLine("Working day");
+}
+else if (dayOFweek == "Saturday" || dayOFweek == "Sunday")
+{
+    Console.WriteLine("Weekend");
+}
+else
+{
+    Console.WriteLine("Error");
+}
