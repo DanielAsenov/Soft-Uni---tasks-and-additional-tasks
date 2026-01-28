336b0bb31c17df93d0686fa970c93a87843a4945
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 20:26:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+
+int length = int.Parse(Console.ReadLine());
+int width = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+double percent = double.Parse(Console.ReadLine());  
+
+
+double volume = length * width * height;
+double volumeLitres = volume / 1000;
+double percentTaken = percent / 100;
+
+double litresRequired = volumeLitres * (1 - percentTaken);
+
+Console.WriteLine(percentTaken);
