4f6a69c3c0c340d177fe00a940b411658382be19
Daniel Asenov <theaerie23@gmail.com>
2026-01-28 21:14:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+string animalType = Console.ReadLine();
+
+if (animalType == "dog")
+{
+    Console.WriteLine("mammal");
+}
+else if (animalType == "crocodile" || animalType == "tortoise" || animalType == "snake")
+{
+    Console.WriteLine("reptile");
+}
+else
+{
+    Console.WriteLine("unknown");
+}
