3b00ddff5c12120aaae9b9ba2eda4eace34f905d
Daniel Asenov <theaerie23@gmail.com>
2026-02-03 14:05:04 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int leftSum = 0;
+int rightSum = 0;
+
+
+for (int i = 0; i < n * 2; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    if (i < n)
+    {
+        leftSum += number;
+    }
+    else
+    {
+        rightSum += number;
+    }
+}
+
+if (leftSum == rightSum)
+{
+    Console.WriteLine($"Yes, sum = {leftSum}");
+}
+else
+{
+    Console.WriteLine($"No, diff = {Math.Abs(leftSum-rightSum)}");
+}
