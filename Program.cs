3598a77aad83ca6334fb98245bbe9b47dbe844a9
Daniel Asenov <theaerie23@gmail.com>
2026-02-07 16:52:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+int periodOfTime = int.Parse(Console.ReadLine());
+
+
+int doctorsAvailable = 7;
+int patientsChecked = 0;
+int patientsUnchecked = 0;
+int patientsTotal = 0;
+
+
+for  (int i = 1; i <= periodOfTime; i++)
+{
+
+    if (i % 3 == 0)
+    {
+        if (patientsUnchecked > patientsChecked)
+        {
+            doctorsAvailable++;
+        }
+
+    }
+
+
+    int patients = int.Parse(Console.ReadLine());
+
+   
+
+
+    if (patients <= doctorsAvailable)
+    {
+        patientsChecked += patients;
+    }
+    else if (patients > doctorsAvailable)
+    {
+        patientsChecked += doctorsAvailable;
+        patientsUnchecked += patients - doctorsAvailable;
+    }
+}
+
+Console.WriteLine($"Treated patients: {patientsChecked}.");
+Console.WriteLine($"Untreated patients: {patientsUnchecked}.");
